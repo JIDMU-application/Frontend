877b55cfb3507f2e928196149ad9c61a018356c2
bhatara007 <bhatara007@gmail.com>
2022-05-14 22:21:35 +0700

    add column-level (h2 database)

--- src/main/java/JIDMU/product/model/User.java
+++ src/main/java/JIDMU/product/model/User.java
@@ -1,12 +1,16 @@
 package JIDMU.product.model;
 
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.UUID;
+
+import JIDMU.product.config.AttributeEncryptor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.Instant;
+
 
 @Data
 @NoArgsConstructor
@@ -16,9 +20,12 @@ public class User {
     @Id
     @GeneratedValue
     private UUID id;
+    @Convert(converter = AttributeEncryptor.class)
     private String username;
     private String password;
+    @Convert(converter = AttributeEncryptor.class)
     private String firstName;
+    @Convert(converter = AttributeEncryptor.class)
     private String lastName;
     private Instant createdAt;
     private String email;
