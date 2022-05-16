bfb3d128c9b4b885904d35dddfe4b6bf08846069
markna551 <auttakrit.w@ku.th>
2022-05-16 15:14:40 +0700

    fix review error

--- src/main/java/JIDMU/product/controller/ReviewController.java
+++ src/main/java/JIDMU/product/controller/ReviewController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
+import org.springframework.validation.BindingResult;
+import javax.validation.Valid;
+
 import java.security.Principal;
 
 
@@ -39,16 +42,21 @@ public class ReviewController {
 
     @GetMapping("/add/{productId}")
     public String getReviewForm(@PathVariable UUID productId,
-                                Model model) {
+                                Model model, ReviewRequest reviewRequest) {
         model.addAttribute("productId", productId);
         return "review-add";
     }
 
     @PostMapping("/add")
-    public String createReview(@ModelAttribute ReviewRequest review,
+    public String createReview(@Valid ReviewRequest review,BindingResult result,
                                Model model, Principal principal) {
         String username = principal.getName();
         review.setUsername(username);
+
+        if (result.hasErrors()) {
+            model.addAttribute("productId", review.getProductId());
+            return "review-add";
+        }
 
         reviewService.createReview(review);
         return "redirect:/review/" + review.getProductId();
--- src/main/java/JIDMU/product/dto/ReviewRequest.java
+++ src/main/java/JIDMU/product/dto/ReviewRequest.java
@@ -2,10 +2,17 @@ package JIDMU.product.dto;
 
 import lombok.Data;
 import java.util.UUID;
+import org.hibernate.validator.constraints.Range;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class ReviewRequest {
     private UUID productId;
+
+
     private String username;
+
+    @NotBlank
     private String reviewText;
 }
